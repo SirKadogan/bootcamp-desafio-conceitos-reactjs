f67aca8a830555f7f69b7b92394acf8d249e5197
SirKadogan <thaleskenne@hotmail.com>
2020-04-16 20:41:49 -0300

    Challenge finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from './services/api';
 
-function App() {
+const App = () => {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const newRepository = {
+      "title": 'Repo ' + repositories.length,
+      "url": "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      "techs": ["Nodejs", "React", "React Native"]
+    }
+    api.post('/repositories', newRepository).then(response => {
+      setRepositories([...repositories, response.data])
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete('/repositories/' + id).then(response => {
+      const filteredRepositories = repositories.filter(repository => repository.id !== id)
+      setRepositories(filteredRepositories)
+    });
   }
 
   return (
-    <div>
+    <div >
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id} style={{ margin: 5 }}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )
+        )}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
